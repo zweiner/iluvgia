a903bc43e057bec842404f92bc6ced294ed9a270
Zach Weiner <146678730+zweiner@users.noreply.github.com>
2025-11-08 17:44:40 -0800

    Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // IMPORTANT: set base to your repo name so GitHub Pages serves assets correctly
 export default defineConfig({
   plugins: [react()],
-  base: '/voice-match/' // <- change if your repo is named differently
+  base: '/iluvgia/' // <- change if your repo is named differently
 })
